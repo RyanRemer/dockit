2bad8fc3c85a0892147a6c8cc2ca596a51b71136
RyanRemer <ryandremer@gmail.com>
2019-02-02 12:55:03 -0700

    models

---
+++ models/project.js
@@ -0,0 +1,9 @@
+class Project{
+    constructor(){
+        self.title = "";
+        self.description = "";
+        self.assigned = [];
+        self.timeSpent = 0.0;
+        self.timeLeft = 0.0;
+    }
+}
---
+++ models/task.js
@@ -0,0 +1,8 @@
+class Task {
+    constructor(){
+        self.title = "";
+        self.assigned = [];
+        self.timeSpent = 0.0;
+        self.timeLeft = 0.0;
+    }
+}
